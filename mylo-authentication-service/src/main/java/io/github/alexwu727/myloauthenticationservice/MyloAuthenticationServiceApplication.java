71c334351be675d242de822dc158ec9c7b3bc652
Alex Wu <alexwu727@gmail.com>
2023-07-04 17:37:18 -0700

    auth service from spring initalizr

---
+++ mylo-authentication-service/src/main/java/io/github/alexwu727/myloauthenticationservice/MyloAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.alexwu727.myloauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyloAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyloAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ mylo-authentication-service/src/test/java/io/github/alexwu727/myloauthenticationservice/MyloAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alexwu727.myloauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyloAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
