be07198add12ddbb1f1f2570f22b222d6d03808a
Alex Wu <alexwu727@gmail.com>
2023-08-02 11:18:17 -0700

    add apis: update, patch, and delete

--- mylo-user-service/src/main/java/io/github/alexwu727/mylouserservice/service/UserServiceImpl.java
+++ mylo-user-service/src/main/java/io/github/alexwu727/mylouserservice/service/UserServiceImpl.java
@@ -53,11 +53,52 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User update(Long id, User user) {
-        return null;
+        Optional<User> userOptional = userRepository.findById(id);
+        if (!userOptional.isPresent()) {
+            throw new UserNotFoundException("User with id " + id + " not found");
+        }
+        if (userRepository.existsByUsername(user.getUsername())) {
+            throw new UsernameAlreadyExistsException("Username " + user.getUsername() + " already exists");
+        }
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new UsernameAlreadyExistsException("Email " + user.getEmail() + " already exists");
+        }
+        User userFromDB = userOptional.get();
+        userFromDB.setUsername(user.getUsername());
+        userFromDB.setEmail(user.getEmail());
+        return userRepository.save(userFromDB);
+    }
+
+    @Override
+    public User patch(Long id, User user) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (!userOptional.isPresent()) {
+            throw new UserNotFoundException("User with id " + id + " not found");
+        }
+        User userFromDB = userOptional.get();
+        if (user.getUsername() != null) {
+            if (userRepository.existsByUsername(user.getUsername())) {
+                throw new UsernameAlreadyExistsException("Username " + user.getUsername() + " already exists");
+            }
+            userFromDB.setUsername(user.getUsername());
+        }
+        if (user.getEmail() != null) {
+            if (userRepository.existsByEmail(user.getEmail())) {
+                throw new UsernameAlreadyExistsException("Email " + user.getEmail() + " already exists");
+            }
+            userFromDB.setEmail(user.getEmail());
+        }
+        if (user.getPassword() != null) {
+            userFromDB.setPassword(user.getPassword());
+        }
+        return userRepository.save(userFromDB);
     }
 
     @Override
     public void delete(Long id) {
-
+        Optional<User> userOptional = userRepository.findById(id);
+        if (!userOptional.isPresent()) {
+            throw new UserNotFoundException("User with id " + id + " not found");
+        }
     }
 }
