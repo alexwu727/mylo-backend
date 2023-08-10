966309256955a11d0c75d3211db5b0ffb5e0d909
Alex Wu <alexwu727@gmail.com>
2023-08-10 11:43:01 -0700

    add AllArgsConstructor and NoArgsConstructor

--- mylo-user-service/src/main/java/io/github/alexwu727/mylouserservice/vo/UserResponse.java
+++ mylo-user-service/src/main/java/io/github/alexwu727/mylouserservice/vo/UserResponse.java
@@ -1,6 +1,5 @@
 package io.github.alexwu727.mylouserservice.vo;
 
-<<<<<<< HEAD
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,11 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-=======
-import lombok.Data;
-
-@Data
->>>>>>> user service - get users, get user by id, get user by username, and register
 public class UserResponse {
     private Long id;
     private String username;
