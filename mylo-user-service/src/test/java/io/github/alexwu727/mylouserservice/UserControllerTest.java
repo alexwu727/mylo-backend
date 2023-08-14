048354c1c5257a65cca8c03ecab86ebd04678693
Alex Wu <alexwu727@gmail.com>
2023-08-14 10:32:37 -0700

    Removed unnecessary import

--- mylo-user-service/src/test/java/io/github/alexwu727/mylouserservice/UserControllerTest.java
+++ mylo-user-service/src/test/java/io/github/alexwu727/mylouserservice/UserControllerTest.java
@@ -13,7 +13,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.*;
 
