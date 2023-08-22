4f17eb7457414f9a4d235bda9a4e7d6735637277
Alex Wu <alexwu727@gmail.com>
2023-08-22 14:52:48 -0700

    configure eureka server

--- mylo-eureka-server/src/main/java/io/github/alexwu727/myloeurekaserver/MyloEurekaServerApplication.java
+++ mylo-eureka-server/src/main/java/io/github/alexwu727/myloeurekaserver/MyloEurekaServerApplication.java
@@ -2,8 +2,10 @@ package io.github.alexwu727.myloeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MyloEurekaServerApplication {
 
 	public static void main(String[] args) {
