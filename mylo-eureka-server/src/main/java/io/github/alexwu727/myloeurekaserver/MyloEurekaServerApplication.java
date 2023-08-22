e22b295602a32dfe7d3f7639191a04f70ec61dad
Alex Wu <alexwu727@gmail.com>
2023-08-22 14:46:40 -0700

    spring initializr

---
+++ mylo-eureka-server/src/main/java/io/github/alexwu727/myloeurekaserver/MyloEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.alexwu727.myloeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyloEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyloEurekaServerApplication.class, args);
+	}
+
+}
---
+++ mylo-eureka-server/src/test/java/io/github/alexwu727/myloeurekaserver/MyloEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alexwu727.myloeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyloEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
