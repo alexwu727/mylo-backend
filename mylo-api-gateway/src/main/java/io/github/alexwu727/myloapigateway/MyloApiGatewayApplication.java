a7def736b836d67f10e0e40599be7f200457c75b
Alex Wu <alexwu727@gmail.com>
2023-08-22 14:21:43 -0700

    spring initalizr

---
+++ mylo-api-gateway/src/main/java/io/github/alexwu727/myloapigateway/MyloApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.alexwu727.myloapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyloApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyloApiGatewayApplication.class, args);
+	}
+
+}
