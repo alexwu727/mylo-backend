97542d76b5370dfb48aa1926535b8e6368cc2d2c
Alex Wu <alexwu727@gmail.com>
2023-10-07 19:57:52 -0700

    implement service discovery

--- mylo-user-service/src/main/java/io/github/alexwu727/mylouserservice/MyloUserServiceApplication.java
+++ mylo-user-service/src/main/java/io/github/alexwu727/mylouserservice/MyloUserServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyloUserServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyloUserServiceApplication.class, args);
 	}
